5f01623977836a56ef3553962b5233f835f51be1
Ragheil <125041628+Ragheil@users.noreply.github.com>
2026-05-20 22:15:26 +0800

    under development 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles/phonipino.css";
-import AppRouter from "./router/AppRouter";
+// import AppRouter from "./router/AppRouter";
 
 // export default function App() {
 //   return <AppRouter />;
